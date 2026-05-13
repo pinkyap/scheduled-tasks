9a442c1609b85e9b0918334edf52a7facec1321f
pinkyap <161711644+pinkyap@users.noreply.github.com>
2026-05-13 10:30:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import random
 import smtplib
 import os
 
+
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
